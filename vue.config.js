8558cb75c188fcedfc54914bb5282d30e8bff3dc
briacl <briac.le_meillat@kerbra.com>
2022-03-27 09:41:40 +0200

    ensure debug code is not published

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-// vue.config.js
+var path = require('path');
 module.exports = {
   runtimeCompiler: true,
+  configureWebpack: {
+    devtool: process.env.CF_PAGES === '1' ? (process.env.__DEBUG__ === '1' ? 'source-map' : false) : 'source-map',
+    mode: process.env.CF_PAGES === '1' ? (process.env.__DEBUG__ === '1' ? 'development' : 'production') : 'development',
+  },
+  chainWebpack(config) {
+    config.resolve.alias.set('vue', path.resolve('./node_modules/vue')); 
+  }
 };
